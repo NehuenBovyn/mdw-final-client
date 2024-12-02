d4f8277cc48017671d861102663d1034c75dec67
Nehuen <bovynnehuen@gmail.com>
2024-12-02 20:42:32 -0300

    Setear la URL del front

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://mdw-final-8qzahnofm-nehuenbovyns-projects.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
